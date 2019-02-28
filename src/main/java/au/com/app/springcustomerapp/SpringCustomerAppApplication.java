55d8d716c1bccb2b3b8e44af8277a7e21b67bcfc
amithn <amith.nmbr@gmail.com>
2019-02-28 22:55:13 +1100

    Adding Environment varialbe to differentiate Canary from Baseline

--- src/main/java/au/com/app/springcustomerapp/SpringCustomerAppApplication.java
+++ src/main/java/au/com/app/springcustomerapp/SpringCustomerAppApplication.java
@@ -1,5 +1,6 @@
 package au.com.app.springcustomerapp;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,11 +8,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.Date;
 
 @SpringBootApplication
 @RestController
 public class SpringCustomerAppApplication {
+
+	@Value("${DEPLOYMENT_TYPE:DONT_KNOW}")
+	private String deploymentType;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCustomerAppApplication.class, args);
@@ -19,12 +24,17 @@ public class SpringCustomerAppApplication {
 
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public Hello helloWorld() {
-		return new Hello("Hello from Cloud Foundry Spinnaker - ACA - Test 3 : " + new Date().toString());
+		return new Hello("Hello from Cloud Foundry Spinnaker - RUNNING AS  : " + deploymentType);
 	}
 
 	@RequestMapping(value = "/customers", method = RequestMethod.POST)
 	public Result add(@RequestBody  Customer customer) {
 		// Save the database
 		return new Result( customer.getName().toLowerCase() , "OK");
+	}
+
+	@PostConstruct
+	public void postConstruct() {
+		System.out.println("deploymentType = " + deploymentType);
 	}
 }
